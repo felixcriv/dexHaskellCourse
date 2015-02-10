9453d51050dfeaa7788443170ef8d4ba2484d11b
Felix Guerrero <felixcriv@gmail.com>
2015-02-10 13:35:44 -0500

    added double, quadruple, factorial and average functions

---
+++ 1. First Steps/part_2.hs
@@ -0,0 +1,22 @@
+--HOW TO USE THIS SCRIPT
+
+-- ghci part_2.hs
+-- quadruple 10
+-- factorial 10
+-- average [1..5]
+
+
+--  double function
+double x = x + x
+
+-- quadruple function calling double function previously defined
+quadruple x = double (double x)
+
+-- faculty function
+-- product is an enviroment function
+factorial n = product [1..n]
+
+-- x `f` y is f x y
+average ns = sum ns `div` length ns
+
+
